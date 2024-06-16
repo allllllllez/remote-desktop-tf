e276a93cc27c5a7af89c0d8a3079f5f011ab86b3
allllllllez <hr.n.nknht@gmail.com>
2024-06-17 03:22:04 +0900

    [fix] yum 実行用に S3 VPC ゲートウェイエンドポイントを追加

--- terraform/module/aws/ec2_remote_instance/main.tf
+++ terraform/module/aws/ec2_remote_instance/main.tf
@@ -6,6 +6,30 @@
 # VPC
 #
 
+# VPC
+module "ec2_remote_instance_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name                 = "ec2_remote_instance_vpc"
+  cidr                 = var.vpc_cidr
+  enable_dns_hostnames = true
+  create_vpc           = true
+  azs                  = var.azs
+  public_subnets = [
+    cidrsubnet(var.vpc_cidr, 8, 0), # default: 10.0.0.0/24 (10.0.0.1 - 10.0.0.255)
+    cidrsubnet(var.vpc_cidr, 8, 1)  # default: 10.0.1.0/24 (10.0.1.1 - 10.0.1.255)
+  ]
+  private_subnets = [
+    cidrsubnet(var.vpc_cidr, 8, 2), # default: 10.0.2.0/24 (10.2.0.1 - 10.2.0.255)
+    cidrsubnet(var.vpc_cidr, 8, 3)  # default: 10.0.3.0/24 (10.3.0.1 - 10.3.0.255)
+  ]
+
+  public_subnet_tags = var.tags
+
+  tags = var.tags
+}
+
 # Security group
 resource "aws_security_group" "ec2_remote_instance_security_group" {
   name        = "ec2_remote_instance_security_group"
@@ -33,37 +57,23 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_remote_instance_security_gro
   ip_protocol       = "27" # rdp cf. https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
 }
 
-# VPC
-module "ec2_remote_instance_vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
-
-  name                 = "ec2_remote_instance_vpc"
-  cidr                 = var.vpc_cidr
-  enable_dns_hostnames = true
-  create_vpc           = true
-  azs                  = var.azs
-  public_subnets = [
-    cidrsubnet(var.vpc_cidr, 8, 0), # default: 10.0.0.0/24 (10.0.0.1 - 10.0.0.255)
-    cidrsubnet(var.vpc_cidr, 8, 1)  # default: 10.0.1.0/24 (10.0.1.1 - 10.0.1.255)
-  ]
-  private_subnets = [
-    cidrsubnet(var.vpc_cidr, 8, 2), # default: 10.0.2.0/24 (10.2.0.1 - 10.2.0.255)
-    cidrsubnet(var.vpc_cidr, 8, 3)  # default: 10.0.3.0/24 (10.3.0.1 - 10.3.0.255)
-  ]
-
-  public_subnet_tags = var.tags
-
+# yum を使用するために S3 VPC ゲートウェイエンドポイントを立てる
+# cf. https://repost.aws/ja/knowledge-center/ec2-al1-al2-update-yum-without-internet
+resource "aws_vpc_endpoint" "ec2_remote_instance_vpc_endpoint_s3" {
+  vpc_id            = module.ec2_remote_instance_vpc.vpc_id
+  service_name      = "com.amazonaws.us-west-2.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = module.ec2_remote_instance_vpc.private_route_table_ids
   tags = var.tags
 }
 
 # 
 # EC2 keypair
 # 
-module "ec2_keypair" {
-  source   = "../ec2_keypair"
-  key_name = var.key_name
-}
+# module "ec2_keypair" {
+#   source   = "../ec2_keypair"
+#   key_name = var.key_name
+# }
 
 data "aws_ami" "ec2_remote_instance_ami" {
   most_recent        = true
@@ -74,7 +84,7 @@ data "aws_ami" "ec2_remote_instance_ami" {
   }
 }
 
-# EC2 (Windows Server インスタンス)
+# EC2
 resource "aws_instance" "ec2_remote_instance" {
   ami           = data.aws_ami.ec2_remote_instance_ami.id
   instance_type = var.instance_type
@@ -85,7 +95,7 @@ resource "aws_instance" "ec2_remote_instance" {
 
   subnet_id                   = module.ec2_remote_instance_vpc.public_subnets[0]
   associate_public_ip_address = true
-  key_name                    = module.ec2_keypair.key_pair_name
+  # key_name                    = module.ec2_keypair.key_pair_name
 
   # インスタンス起動時に実行するscript
   user_data = file(var.user_data_script)
